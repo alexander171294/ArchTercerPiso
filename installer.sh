cccc45d69a2a3fe28fd86613885541109b48cad1
Alexander Eberle <alexander171294@gmail.com>
2022-07-01 12:01:33 -0300

    loadkeys

--- installer.sh
+++ installer.sh
@@ -5,6 +5,7 @@
 # @brief Entrance script that launches children scripts for each phase of installation.
 
 # Find the name of the folder the scripts are in
+loadkeys es
 set -a
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
