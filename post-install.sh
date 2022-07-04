f0bae75227db432dbe76b751afd4d1d8631f6502
Alexander Eberle <alexander171294@gmail.com>
2022-07-04 18:04:43 -0300

    configs

--- post-install.sh
+++ post-install.sh
@@ -12,8 +12,12 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
-cp ~/TercerPiso/configs/xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-cp ~/TercerPiso/configs/keyboard-layout.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
+cp ~/TercerPiso/configs/xconf/xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+cp ~/TercerPiso/configs/xconf/xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+cp ~/TercerPiso/configs/xconf/keyboard-layout.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
+cp ~/TercerPiso/configs/xconf/xfce4-power-manager.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+cp ~/TercerPiso/configs/xconf/xfwm4.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+cp ~/TercerPiso/configs/xconf/xsettings.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 mkdir -p $HOME/.config/xfce4/terminal
 cp ~/TercerPiso/configs/terminalrc.txt $HOME/.config/xfce4/terminal/terminalrc
 feh --bg-fill /usr/share/backgrounds/xfce/wallpaper.png
