338aadec5b9559391f2d9a83148d5216b355d73d
Alexander Eberle <alexander171294@gmail.com>
2022-07-04 16:10:08 -0300

    fixing postinstall

--- post-install.sh
+++ post-install.sh
@@ -11,8 +11,8 @@ echo -ne "
                        Post-install.sh - 0000101
 -------------------------------------------------------------------------
 "
-mkdir -p /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml
-cp ~/TercerPiso/configs/xfce4-keyboard-shortcuts.xml /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-mkdir -p /home/$USERNAME/.config/xfce4/terminal
-cp ~/TercerPiso/configs/terminalrc.txt /home/$USERNAME/.config/xfce4/terminal/terminalrc
+mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
+cp ~/TercerPiso/configs/xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+mkdir -p $HOME/.config/xfce4/terminal
+cp ~/TercerPiso/configs/terminalrc.txt $HOME/.config/xfce4/terminal/terminalrc
 feh --bg-fill /usr/share/backgrounds/xfce/wallpaper.png
