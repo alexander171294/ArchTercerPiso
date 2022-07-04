9513a3c48980172d7ec45599405f18453e19b964
Alexander Eberle <alexander171294@gmail.com>
2022-07-04 14:46:44 -0300

    post install

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,16 @@
+echo -ne "
+-------------------------------------------------------------------------
+ _____                           ___ _           
+/__   \___ _ __ ___ ___ _ __    / _ (_)___  ___  
+  / /\/ _ \ '__/ __/ _ \ '__|  / /_)/ / __|/ _ \ 
+ / / |  __/ | | (_|  __/ |    / ___/| \__ \ (_) |
+ \/   \___|_|  \___\___|_|    \/    |_|___/\___/   
+
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                       Post-install.sh - 0000101
+-------------------------------------------------------------------------
+"
 mkdir -p /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml
 cp ~/TercerPiso/configs/xfce4-keyboard-shortcuts.xml /home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 mkdir -p /home/$USERNAME/.config/xfce4/terminal
